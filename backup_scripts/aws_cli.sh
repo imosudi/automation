9a00b39b7e0cc2c34a17f31f0981dd026e8849dc
imosudi <imosudi@gmail.com>
2017-08-20 20:08:18 +0100

    Project Update -Sun 20 Aug 2017 20:08:18 WAT

--- backup_scripts/aws_cli.sh
+++ backup_scripts/aws_cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws configure set aws_access_key_id jdjdjf 
-aws configure set aws_secret_access_key jfjfjfjk 
+aws configure set aws_access_key_id jhjhjhsdjh 
+aws configure set aws_secret_access_key jhjhsjhjh 
 aws configure set output json
 aws configure set region us-west-2
 
